4295c6b73639d2946697f8dbb70be9e7909bf8d9
Mateo <108905809+MateoPalotta@users.noreply.github.com>
2023-07-30 20:10:39 -0300

    Add files via upload

---
+++ Repaso/src/modelo/Objetos.java
@@ -0,0 +1,17 @@
+package modelo;
+
+public class Objetos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona persona = new Persona("Mateo", 17);
+		persona.mostrar();
+		System.out.println(" ");
+		
+		Empleado empleado = new Empleado("Mateo", 17, 2500);
+		empleado.mostrar();
+		System.out.println(" ");
+		
+	}
+
+}
